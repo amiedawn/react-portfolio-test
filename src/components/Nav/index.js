b8c69547937ac8f853e497411fec9650cba19c6c
amiedawn <amie.d.carroll@gmail.com>
2021-02-24 00:15:59 -0700

    basic about and nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function Nav() {
+  
+
+  return (
+    <header>
+      <h2>
+        <a href="/">AMIE CARROLL</a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About Me</a>
+          </li>
+          <li className="mx-2">
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          <li className="mx-2">
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
