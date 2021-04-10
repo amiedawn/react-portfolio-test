b676815fb751b85691f721b15731b01681d4ad41
amiedawn <amie.d.carroll@gmail.com>
2021-04-10 13:18:51 -0600

    modify contact error message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,13 +25,13 @@ function ContactForm() {
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
-        setErrorMessage("Your email is invalid.");
+        setErrorMessage("Your email is invalid!");
       } else {
         setErrorMessage("");
       }
     } else {
       if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
+        setErrorMessage(`${e.target.name} is required!`);
       } else {
         setErrorMessage("");
       }
