c18543b167d383c3bc8a90037b69a65da5ff9c8d
amiedawn <amie.d.carroll@gmail.com>
2021-06-19 23:32:14 -0600

    add thumbnail pic

--- src/components/DownloadResume.js
+++ src/components/DownloadResume.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import myResume from "../assets/images/resume.jpg";
+import resume from "./../assets/images/resume.pdf"
 import Button from "react-bootstrap/Button";
 
 class DownloadResume extends Component {
   render() {
     return (
       <Button
-        href={myResume}
-        download="resume.jpg"
         className="ml-3 mr-6"
         style={{
           backgroundColor: "#be8180",
           color: "#122240",
-        
         }}
+          href={resume} download="resume.pdf"
       >
         Download Resume Here
       </Button>
